09c715a735dc05813972dcfcb2cfe2079308eec2
RyanPerez <43544001+RyanPerez@users.noreply.github.com>
2019-03-20 10:06:40 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#Button1").click(function(){
+$(".Eat").hide(); 
+});
+
+
+
+//Spell #2
+
+$("#button2").click(function(){
+$(".money").fadeToggle(); 
+});
+
+
+
+//Spell #3
+
+$("#button3").click(function(){
+$(".diamonds").slideToggle(); 
+});
+
+
+
